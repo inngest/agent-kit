df435ba56d650b0d6f6c12cf0b93a0670ea2b98d
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-11-20 22:10:46 -0800

    Add step to tool handler func

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,17 @@
+import { z } from "zod";
+import {
+  type GetStepTools,
+  type Inngest,
+} from "inngest";
 import { Agent } from "./agent";
 import { Network } from "./network";
 import { InferenceResult, InternalNetworkMessage } from "./state";
 import { MaybePromise } from "./util";
 
-export type Tool = {
+export type Tool<T extends z.ZodTypeAny> = {
   name: string;
   description?: string;
-  parameters: Record<string, unknown>; // TODO: JSON Schema Type.
+  parameters: T;
 
   // TODO: Handler input types based off of JSON above.
   //
@@ -14,8 +19,14 @@ export type Tool = {
   // the current agent and network.  This allows tools to reference and schedule
   // future work via the network, if necessary.
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  handler: (input: Record<string, any>, agent: Agent, network?: Network) => any;
+  handler: (input: z.infer<T>, opts: ToolHandlerArgs) => Promise<any>;
 };
+
+export type ToolHandlerArgs = {
+  agent: Agent;
+  network?: Network;
+  step: GetStepTools<Inngest.Any>;
+}
 
 export interface BaseLifecycleArgs {
   // Agent is the agent that made the call.
@@ -25,38 +36,12 @@ export interface BaseLifecycleArgs {
 }
 
 export interface ResultLifecycleArgs extends BaseLifecycleArgs {
-  call: InferenceResult;
+  result: InferenceResult;
 }
 
 export interface BeforeLifecycleArgs extends BaseLifecycleArgs {
   // input is the user request for the entire agentic operation.
   input?: string;
-  instructions: InternalNetworkMessage[];
+  system: InternalNetworkMessage[];
   history?: InternalNetworkMessage[];
-}
-
-/**
- * InferenceLifecycle are lifecycle hooks shared between agents and networks.
- */
-export interface InferenceLifecycle {
-  /**
-   * Before allows you to intercept and modify the input prompt for a given agent,
-   * or prevent the agent from being called altogether by throwing an error.
-   *
-   * This receives the full agent prompt.  If this is a networked agent, the agent
-   * will also receive the network's history which will be concatenated to the end
-   * of the prompt when making the inference request.
-   *
-   */
-  beforeInfer?: (args: BeforeLifecycleArgs) => MaybePromise<{
-    instructions: InternalNetworkMessage[];
-    history: InternalNetworkMessage[];
-  }>;
-
-  /**
-   * afterTools is called after an agent invokes tools as specified by the inference call. The
-   * returned InferenceResult will be saved to network history, if the agent is part of the network.
-   *
-   */
-  afterTools?: (args: ResultLifecycleArgs) => MaybePromise<InferenceResult>;
 }
