d82bea58c7e1b0128a04b8a2f27501d4e50bda0d
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-12-10 18:55:48 -0800

    Add example swebench using tree-sitter to edit code.

--- examples/swebench/inngest.ts
+++ examples/swebench/inngest.ts
@@ -1,17 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import fs from "fs";
 import { execSync } from 'child_process';
+import { z } from "zod";
 import {
   createAgent,
   createNetwork,
   createTool,
-  defaultRoutingAgent,
-  openai,
   anthropic,
   State,
 } from "../../src/index";
+import { extractClassAndFns, listFilesTool, readFileTool, replaceClassMethodTool } from "./tools/tools";
 import { Inngest, EventSchemas } from "inngest";
-import { z } from "zod";
 
 export const inngest = new Inngest({
   id: "agents",
@@ -58,20 +57,28 @@ export const fn = inngest.createFunction(
     const model = anthropic({
       model: "claude-3-5-haiku-latest",
       max_tokens: 1000,
-      step: step as any, // TODO: typing
+      step: step as any,
     });
 
     const state = new State();
     state.kv.set("repo", event.data.repo);
-
-    console.log("running network");
 
     const network = createNetwork({
       agents: [planningAgent.withModel(model), editingAgent.withModel(model)],
       defaultModel: model,
       state,
     });
-    await network.run(event.data.problem_statement);
+    await network.run(event.data.problem_statement, (opts) => {
+      if (opts.network.state.kv.get("done")) {
+        // We're done editing.
+        return;
+      }
+
+      if (opts.network.state.kv.get("plan") !== undefined) {
+        return editingAgent.withModel(model);
+      }
+      return planningAgent.withModel(model);
+    });
   },
 );
 
@@ -80,45 +87,21 @@ const planningAgent = createAgent({
   name: "Planner",
   description: "Plans the code to write and which files should be edited",
   tools: [
-
-    createTool({
-      name: "list_files",
-      description: "Lists all files within the project, returned as a JSON string containign the path to each file",
-      handler: async (_input, opts) => {
-        // NOTE:  In this repo, all files are stored in "./opt/" as the prefix.
-        const path = "./opt/" + opts.network?.state.kv.get("repo")
-        const files = await opts.step.run("read files", () => fs.readdirSync(path, { recursive: true }));
-        opts.network && opts.network.state.kv.set("files", files);
-        return files;
-      },
-    }),
-
-    createTool({
-      name: "read_file",
-      description: "Reads a single file given its filename, returning its contents",
-      parameters: z.object({
-        filename: z.string(),
-      }),
-      handler: async ({ filename }, opts) => {
-        // NOTE:  In this repo, all files are stored in "./opt/" as the prefix.
-        const path = "./opt/" + opts.network?.state.kv.get("repo")
-        const content = await opts.step.run(`list file: ${filename}`, () => {
-          return fs.readFileSync(path + "/" + filename).toString();
-        })
-
-        // Set state for the filename.
-        opts.network?.state.kv.set("file:" + filename, content);
-        return content;
-      },
-    }),
+    listFilesTool,
+    readFileTool,
+    extractClassAndFns,
 
     createTool({
       name: "create_plan",
-      description: "Describe a formal plan for how to fix the issue.  Describe the plan.  Describe which files to edit.  Including reasoning.",
+      description: "Describe a formal plan for how to fix the issue, including which files to edit and reasoning.",
       parameters: z.object({
-        description: z.string(),
-        reasoning: z.string(),
-        edit_files: z.array(z.string()).describe("The filenames to edit"),
+        thoughts: z.string(),
+        plan_details: z.string(),
+        edits: z.array(z.object({
+          filename: z.string(),
+          idea: z.string(),
+          reasoning: z.string(),
+        }))
       }),
 
       handler: async (plan, opts) => {
@@ -126,7 +109,7 @@ const planningAgent = createAgent({
         await opts.step.run("plan created", () => plan);
         opts.network?.state.kv.set("plan", plan);
       },
-    })
+    }),
   ],
 
   system: (network) => `
@@ -137,6 +120,10 @@ const planningAgent = createAgent({
 
     Your plan will be worked on and implemented after you create it.   You MUST create a plan to
     fix the issue.  Be thorough. Think step-by-step using available tools.
+
+    Techniques you may use to create a plan:
+    - Read entire files
+    - Find specific classes and functions within a file
   `,
 })
 
@@ -149,52 +136,41 @@ const editingAgent = createAgent({
   name: "Editor",
   description: "Edits code by replacing contents in files, or creating new files with new code.",
   tools: [
+    extractClassAndFns,
+    replaceClassMethodTool,
+    readFileTool,
+
     createTool({
-      name: "patch_file",
-      description: "Updates the contents of a single file.  This replaces existing contents in the given line ranges with new content provided.  The line numbers are 1-indexed, starting at 1.",
-      parameters: z.object({
-        filename: z.string(),
-        content: z.string(),
-        line_range_start: z.number(),
-        line_range_end: z.number(),
-      }),
-      handler: async ({ filename, content, line_range_start, line_range_end }, opts) => {
-        // NOTE:  In this repo, all files are stored in "./opt/" as the prefix.
-        const path = "./opt/" + opts.network?.state.kv.get("repo")
-        const lines = fs.readFileSync(path + "/" + filename).toString().split("\n");
-
-        const updated = lines.reduce((updated, line, idx) => {
-          const beforeRange = idx < (line_range_start-1);
-          const isRange = idx === (line_range_start-1);
-          const afterRange = idx >= (line_range_end);
-
-          if (beforeRange || afterRange) {
-            return [...updated, line];
-          }
-
-          return isRange ? [...updated, content] : updated;
-        }, [] as string[]).join("\n");
-
-        await opts.step.run("updated content", () => updated);
-
-        fs.writeFileSync(path + "/" + filename, updated);
-
-        // TODO: Mark the plan as done.
-
-        return updated;
+      name: "done",
+      description: "Saves the current project and finishes editing",
+      handler: (_input, opts) => {
+        opts.network?.state.kv.delete("plan");
+        opts.network?.state.kv.set("done", true);
+        return "Done editing";
       },
     }),
   ],
   lifecycle: {
+
+    // The editing agent is only enabled once we have a plan.
     enabled: (opts) => {
-      // The editing agent is only enabled once we have a plan.
       return opts.network?.state.kv.get("plan") !== undefined;
     },
-    onStart: ({ prompt }) => {
-      // Do not return the current history.
-      return { prompt, history: [], stop: false };
+
+    // onStart is called when we start inference.  We want to update the history here to remove
+    // things from the planning agent.  We update the system prompt to include details from the
+    // plan via network state.
+    onStart: ({ agent, prompt, network }) => {
+
+      const history = (network?.state.results || []).
+        filter(i => i.agent === agent). // Return the current history from this agent only.
+        map(i => i.output.concat(i.toolCalls)). // Only add the output and tool calls to the conversation history
+        flat();
+
+      return { prompt, history, stop: false };
     },
   },
+
   system: (network) => `
     You are an expert Python programmer working on a specific project: ${network?.state.kv.get("repo")}.  You have been
     given a plan to fix the given issue supplied by the user.
@@ -204,14 +180,11 @@ const editingAgent = createAgent({
       ${JSON.stringify(network?.state.kv.get("plan"))}
     </plan>
 
-    The content for each file to edit in the plan:
-    ${(network?.state.kv.get("plan")?.edit_files || []).forEach((filename: string) => {
-      return `<content filename="${filename}">${network?.state.kv.get("file:" + filename)}</content>\n`;
-    })}
-
     You MUST:
       - Understand the user's request
       - Understand the given plan
       - Write code using the tools available to fix the issue
+
+    Once the files have been edited and you are confident in the updated code, you MUST finish your editing via calling the "done" tool.
   `,
 })
