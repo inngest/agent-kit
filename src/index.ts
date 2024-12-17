ded7b314c97d30676b235163c758f3837407f41a
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-12-17 10:41:49 -0800

    Update imports (#26)
    * Upgrade `inngest` to `3.28.0`
    
    * Switch to `getDefaultRoutingAgent()`
    
    ---------
    
    Co-authored-by: Jack Williams <1736957+jpwilliams@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from "./agent";
 export * from "./model";
 export * from "./network";
+export * from "./networkRun";
 export * from "./state";
 export * from "./types";
 export * from "./util";
