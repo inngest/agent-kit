4658b8e0c2dee6c1b363bcb5ffe028df4d9239f1
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-01-08 11:42:15 +0000

    Move `createServer()` to a separate `@inngest/agent-kit/server` export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from "./agent";
 export * from "./model";
 export * from "./network";
 export * from "./networkRun";
-export * from "./server";
 export * from "./state";
 export * from "./types";
 export * from "./util";
