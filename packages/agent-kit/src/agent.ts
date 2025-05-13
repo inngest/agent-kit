43a0745b9ca95d3d3f113fa21fd3f60991abf29f
ted <werbel.ted@gmail.com>
2025-05-13 17:06:18 -0400

    bugfix: duplicate calls to listMCPTools (#145)
    * bugfix: duplicate calls to listMCPTools
    
    test case for this in another PR#129 (to be closed)
    
    * removed redundant this.listMCPTools call
    
    * Add changeset for listMCPTools fix
    
    * added dsstore to gitignore
    
    ---------
    
    Co-authored-by: Mahesh <mahesh.tiyyagura@medable.com>
    Co-authored-by: Ted Werbel <tedwerbel@Teds-MacBook-Pro.local>

--- packages/agent-kit/src/agent.ts
+++ packages/agent-kit/src/agent.ts
@@ -395,16 +395,12 @@ export class Agent<T extends StateData> {
   // initMCP fetches all tools from the agent's MCP servers, adding them to the tool list.
   // This is all that's necessary in order to enable MCP tool use within agents
   private async initMCP() {
-    if (
-      !this.mcpServers ||
-      this._mcpClients.length === this.mcpServers.length
-    ) {
+    if (!this.mcpServers || this._mcpClients.length >= this.mcpServers.length) {
       return;
     }
 
     const promises = [];
     for (const server of this.mcpServers) {
-      await this.listMCPTools(server);
       promises.push(this.listMCPTools(server));
     }
 
@@ -416,6 +412,7 @@ export class Agent<T extends StateData> {
    */
   private async listMCPTools(server: MCP.Server) {
     const client = await this.mcpClient(server);
+    this._mcpClients.push(client);
     try {
       const results = await client.request(
         { method: "tools/list" },
@@ -508,7 +505,6 @@ export class Agent<T extends StateData> {
       // The transport closed.
       console.warn("mcp server disconnected", server, e);
     }
-    this._mcpClients.push(client);
     return client;
   }
 }
