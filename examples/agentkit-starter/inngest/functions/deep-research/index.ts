5ea39c644f7393a6b3e8a7b9ecdde35a9452b66a
Ted Werbel <werbel.ted@gmail.com>
2025-06-17 16:25:57 -0400

    clean up

--- examples/agentkit-starter/inngest/functions/deep-research/index.ts
+++
@@ -1,4 +0,0 @@
-// Export the v2 research agents
-export { stagingAgent } from "./staging-agent";
-export { reasoningAgent } from "./reasoning-agent";
-export { reportingAgent } from "./reporting-agent";
